16ff7a8e7b599613218215e376f9cf1b472f3c12
Mark Hamstra <hello@markhamstra.com>
2018-11-20 15:27:56 +0100

    Code style

--- _build/digitalsignage/index.php
+++ _build/digitalsignage/index.php
@@ -1,24 +1,23 @@
 <?php
 
-    /**
-     * Package Builder
-     *
-     * Copyright 2017 by Oene Tjeerd de Bruin <modx@oetzie.nl>
-     */
+/**
+ * Package Builder
+ *
+ * Copyright 2017 by Oene Tjeerd de Bruin <modx@oetzie.nl>
+ */
 
-    require_once __DIR__ . '/packagebuilder.class.php';
-    if (!defined('MODX_CORE_PATH')) {
-        require_once dirname(dirname(__DIR__)) . '/config.core.php';
-    }
+require_once __DIR__ . '/packagebuilder.class.php';
+if (!defined('MODX_CORE_PATH')) {
+    require_once dirname(dirname(__DIR__)) . '/config.core.php';
+}
 
-    $modx = isset($modx) ? $modx : null;
-    $package = new PackageBuilder(
-        __DIR__ . '/package.json',
-        dirname(dirname(__DIR__)) . '/core/',
-        null,
-        $modx
-    );
+$modx = isset($modx) ? $modx : null;
+$package = new PackageBuilder(
+    __DIR__ . '/package.json',
+    dirname(dirname(__DIR__)) . '/core/',
+    null,
+    $modx
+);
 
-    $package->createPackageZip();
+$package->createPackageZip();
 
-?>
